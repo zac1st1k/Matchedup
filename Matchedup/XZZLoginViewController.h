52e825b9be103a6929b11ac5638b0d4b0d63d008
zac1st1k <zacfirst@gmail.com>
2015-01-11 23:55:44 +1100

    + Setup storyboard

---
+++ Matchedup/XZZLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  XZZLoginViewController.h
+//  Matchedup
+//
+//  Created by Zac on 11/01/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XZZLoginViewController : UIViewController
+
+@end
---
+++ Matchedup/XZZLoginViewController.m
@@ -0,0 +1,45 @@
+//
+//  XZZLoginViewController.m
+//  Matchedup
+//
+//  Created by Zac on 11/01/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import "XZZLoginViewController.h"
+
+@interface XZZLoginViewController ()
+
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+
+@end
+
+@implementation XZZLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark - IBAction
+
+- (IBAction)loginButtonPressed:(id)sender {
+}
+
+
+@end
