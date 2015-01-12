d0b21eed8b89fe0186e243f1cea71b7d97ee3488
zac1st1k <zacfirst@gmail.com>
2015-01-12 21:33:04 +1100

    + Profile picture to second view + Query profile picture from Parse

--- Matchedup/SecondViewController.m
+++ Matchedup/SecondViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "SecondViewController.h"
+#import "XZZConstants.h"
+#import <Parse/Parse.h>
+#import <FacebookSDK/FacebookSDK.h>
+#import <ParseFacebookUtils/PFFacebookUtils.h>
+
 
 @interface SecondViewController ()
+
+@property (strong, nonatomic) IBOutlet UIImageView *profilePictureImageView;
 
 @end
 
@@ -17,6 +24,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    PFQuery *query = [PFQuery queryWithClassName:kXZZPhotoClassKey];
+    [query whereKey:kXZZPhotoUserKey equalTo:[PFUser currentUser]];
+    
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if ([objects count] > 0) {
+            PFObject *photo = objects[0];
+            PFFile *pictureFile = photo[kXZZPhotoPictureKey];
+            [pictureFile getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+                self.profilePictureImageView.image = [UIImage imageWithData:data];
+            }];
+        }
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning {
