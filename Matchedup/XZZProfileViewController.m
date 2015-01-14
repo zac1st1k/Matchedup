de9529ee9f56edd135e3dab506a62f12889e85fe
zac1st1k <zacfirst@gmail.com>
2015-01-14 23:48:26 +1100

    + Setup ProfileViewController

---
+++ Matchedup/XZZProfileViewController.m
@@ -0,0 +1,43 @@
+//
+//  XZZProfileViewController.m
+//  Matchedup
+//
+//  Created by Zac on 14/01/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import "XZZProfileViewController.h"
+
+@interface XZZProfileViewController ()
+
+@property (strong, nonatomic) IBOutlet UIImageView *profilePictureImageView;
+@property (strong, nonatomic) IBOutlet UILabel *locationLabel;
+@property (strong, nonatomic) IBOutlet UILabel *ageLabel;
+@property (strong, nonatomic) IBOutlet UILabel *statusLabel;
+@property (strong, nonatomic) IBOutlet UILabel *tagLineLabel;
+
+@end
+
+@implementation XZZProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
