46a8d46df458f8b65c9e0b7be42e19a85862d72e
zac1st1k <zacfirst@gmail.com>
2015-01-15 22:57:22 +1100

    + Download Home Photo

--- Matchedup/XZZHomeViewController.m
+++ Matchedup/XZZHomeViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "XZZHomeViewController.h"
+#import <Parse/Parse.h>
 
 @interface XZZHomeViewController ()
 @property (strong, nonatomic) IBOutlet UIBarButtonItem *chatBarButtonItem;
@@ -18,12 +19,18 @@
 @property (strong, nonatomic) IBOutlet UIButton *likeButton;
 @property (strong, nonatomic) IBOutlet UIButton *infoButton;
 @property (strong, nonatomic) IBOutlet UIButton *dislikeButton;
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+
 
 - (IBAction)chatBarButtonItemPressed:(UIBarButtonItem *)sender;
 - (IBAction)settingsBarButtonPressed:(UIBarButtonItem *)sender;
 - (IBAction)likeButtonPressed:(UIButton *)sender;
 - (IBAction)infoButtonPressed:(UIButton *)sender;
 - (IBAction)dislikeButtonPressed:(UIButton *)sender;
+
+@property (strong, nonatomic) NSArray *photos;
+@property (strong, nonatomic) PFObject *photo;
+@property (nonatomic) int currentPhotoIndex;
 
 @end
 
@@ -32,6 +39,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self.activityIndicator startAnimating];
+    self.likeButton.enabled = NO;
+    self.dislikeButton.enabled = NO;
+    self.infoButton.enabled = NO;
+    self.currentPhotoIndex = 0;
+    PFQuery *query = [PFQuery queryWithClassName:@"Photo"];
+    [query includeKey:@"user"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (!error) {
+            self.photos = objects;
+            NSLog(@"photos are %@", self.photos);
+            [self queryForCurrentPhotoIndex];
+            self.activityIndicator.hidden = YES;
+        }
+        else {
+            NSLog(@"%@", error);
+        }
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,14 +65,14 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 #pragma mark - IBActions
 
@@ -64,5 +89,22 @@
 }
 
 - (IBAction)dislikeButtonPressed:(UIButton *)sender {
+}
+
+#pragma mark - Helper Methods
+
+- (void)queryForCurrentPhotoIndex
+{
+    if ([self.photos count] > 0) {
+        self.photo = self.photos[self.currentPhotoIndex];
+        PFFile *file = self.photo[@"image"];
+        [file getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+            if (!error) {
+                UIImage *image = [UIImage imageWithData:data];
+                self.photoImageView.image = image;
+            }
+            else NSLog(@"%@", error);
+        }];
+    }
 }
 @end
